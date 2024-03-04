d768f48e84c90d5432fb885af1aec5df1215a5d6
Thin Creator 3483 <102002463+SaxbyMod@users.noreply.github.com>
2024-03-04 18:21:14 -0500

    Add Cost, Token, and Flavor to CTI
    Also changed it to call the correct name

--- extra/stoatLordCardStuff.js
+++ extra/stoatLordCardStuff.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch")
 
 var imfJson = {
-    ruleset: "Stoat Lord Card Thingymagik",
+    ruleset: "Custom TCG Inscryption",
     cards: [],
     sigils: {},
 }
@@ -30,6 +30,33 @@ async function load() {
         cardFormated.cost = card["Cost"]
         cardFormated.attack = card["Power"]
         cardFormated.health = card["Health"]
+
+        // parsing cost
+        card["Cost"] = card["Cost"]
+            .replace("bones", "bone")
+            .replace("sapphires", "sapphire")
+            .replace("rubies", "ruby")
+            .replace("prisms", "prism")
+        for (let cost of card["Cost"].split(",")) {
+            cost = cost.trim().toLowerCase()
+            let temp = cost.split(" ")
+            if (cost.includes("shattered")) {
+                if (!cardFormated["shattered"]) cardFormated["shattered"] = []
+                for (let i = 0; i < temp[0]; i++) {
+                    cardFormated["shattered"].push(`shattered_${temp[2]}`)
+                }
+            } else if (["sapphire", "ruby", "emerald", "prism"].some((i) => cost.includes(i))) {
+                if (!cardFormated["gem"]) cardFormated["gem"] = []
+                for (let i = 0; i < temp[0]; i++) {
+                    cardFormated["gem"].push(temp[1])
+                }
+            } else if (temp.length > 0) {
+                cardFormated[temp[1]] = parseInt(temp[0])
+            }
+        }
+
+        if (card["Token"]) cardFormated["token"] = card["Token"]
+        cardFormated["description"] = card["Flavor"]
 
         cardFormated.sigils = [
             card["Sigil 1"] ?? "",
